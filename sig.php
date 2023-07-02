3f1027068267f31befb8884d582612cd1790c459
mpgayathri001 <137909957+mpgayathri001@users.noreply.github.com>
2023-07-02 19:43:47 +0530

    Add files via upload

---
+++ sig.php
@@ -0,0 +1,23 @@
+<?php
+error_reporting(1);
+$name=$_POST['u'];
+$pass=$_POST['v'];
+$email=$_POST['b'];
+$con=new mysqli('localhost','root','','log');
+if(isset($_POST['SUBMIT']))
+{
+$sql="INSERT INTO logi(username,password) value ('$name','$pass')";
+$con->query($sql);
+echo "inserted";
+$to = $email;
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n password =" . $pass;
+$headers = "From: mpgayathri001@gmail.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+
+header("Location: https://mpgayathri001.github.io/myweb-f/login.html");
+}
+}
+?>
