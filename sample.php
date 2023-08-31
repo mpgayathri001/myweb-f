244b4bccb7ea2e3a759e1c991a3646c97f0e53a6
mpgayathri001 <137909957+mpgayathri001@users.noreply.github.com>
2023-08-31 08:59:40 -0700

    Add files via upload

---
+++ sample.php
@@ -0,0 +1,16 @@
+<?php
+error_reporting(1);
+$name=$_POST['u'];
+$rno=$_POST['v'];
+$con=new mysqli('localhost','root','','log');
+if(isset($_POST['SUBMIT']))
+{
+$sql="INSERT INTO lo(usesrname,password)VALUES('$name','$rno')";
+$con->query($sql);
+header("Location: https://mpgayathri001.github.io/myweb-f/login.html");
+}
+
+
+
+
+?>
