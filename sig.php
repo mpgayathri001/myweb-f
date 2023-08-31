f4b804aa4fda0122561082e366f15137bd8c5741
mpgayathri001 <137909957+mpgayathri001@users.noreply.github.com>
2023-08-30 19:19:40 -0700

    Update sig.php

--- sig.php
+++ sig.php
@@ -1,17 +1,8 @@
 <?php
-error_reporting(1);
-$name=$_POST['u'];
-$pass=$_POST['v'];
-$email=$_POST['b'];
-$con=new mysqli('localhost','root','','log');
-if(isset($_POST['SUBMIT']))
-{
-$sql="INSERT INTO logi(username,password) value ('$name','$pass')";
-$con->query($sql);
-echo "inserted";
+
 
 
 header("Location: https://mpgayathri001.github.io/myweb-f/login.html");
-}
-}
+
+
 ?>
